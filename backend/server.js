ac7654f91fed84106b6974a6afad8b1d6f422e2d
ferdous85 <ferdous85@gmail.com>
2021-08-26 11:36:13 +0600

    note route

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,23 @@ const cors = require('cors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 
+const notes = require('./data/notes')
+
 
 const app = express()
 dotenv.config()
 
 app.get('/', (req, res)=>{
   res.json({msg:'hello world'})
+})
+
+app.get('/notes/:id', (req, res)=>{
+  const note = notes.find((n)=>n._id === req.params.id)  
+  res.send(note)
+})
+
+app.get('/notes', (req, res)=>{  
+  res.send(notes)
 })
 
 const PORT = process.env.PORT || 5000
